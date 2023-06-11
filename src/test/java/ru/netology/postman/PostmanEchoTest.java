fe531309b3455fc0ac7752cd5b586c610a0c424e
Yakovleva Anna <Yakovleva_A@kadis.ru>
2023-06-11 23:22:08 +0300

    bug fix

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("sum data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
